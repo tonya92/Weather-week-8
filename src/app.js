2c75e082fa832a89a89a26863462712cfce062d5
Tonya <aorynenko@gmail.com>
2022-06-08 16:19:17 +0300

    added 6 days forecast

--- src/app.js
+++ src/app.js
@@ -42,6 +42,56 @@ function formatDate(timestamp) {
   };
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
+      <div class="col-2">
+         <div class="weather-forecast-date">
+             ${formatDay(forecastDay.dt)}
+   </div>
+      <img src="http://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png" alt="" width="50">
+         <div class="weather-forecast-temperature">
+            <span class="weather-forecast-temperature-max">
+               ${Math.round(forecastDay.temp.max)}°
+             </span>
+           <span class="weather-forecast-temperature-min">
+               ${Math.round(forecastDay.temp.min)}°
+     </span>
+
+       </div>
+    </div>
+ 
+    `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "e31b527c9062c53c4be1268eb629a005";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function fxormatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
@@ -76,6 +126,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "e31b527c9062c53c4be1268eb629a005";
